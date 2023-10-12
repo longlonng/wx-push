7abe98d7eb747c9e65ffc36c58c94a3999644fc1
longlonng <147294659+longlonng@users.noreply.github.com>
2023-10-12 13:51:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx4c89b56dfd33cb3e",
   appsecret: "1862f1d569a5f6179ae9b8c461d704d3",
   template_id: "ItBfmzvaWVtxAO1b8YZ1nEk2ODj0e9K3Zbu1nKTBExQ",
-  touser: "ocTcm6d8_hCdcpp89E0UsZeAtRuU",
+  touser: "ocTcm6cglarRkYmkrF_0ePh8D1z8",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "新乡",
